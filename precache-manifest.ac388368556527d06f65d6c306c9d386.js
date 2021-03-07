4a437385fa9347d430154ef83e5def0136b993bc
surajk95 <surajkukati95@gmail.com>
2021-03-07 22:55:25 +0530

    Updates

---
+++ precache-manifest.ac388368556527d06f65d6c306c9d386.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8352632230e926ded11e84f1507ab502",
+    "url": "/player-of-numbers/index.html"
+  },
+  {
+    "revision": "37578e75548c3146e84c",
+    "url": "/player-of-numbers/static/css/main.27bd141a.chunk.css"
+  },
+  {
+    "revision": "09340e3658338ee9c096",
+    "url": "/player-of-numbers/static/js/2.9a1bbd62.chunk.js"
+  },
+  {
+    "revision": "37578e75548c3146e84c",
+    "url": "/player-of-numbers/static/js/main.9c82284d.chunk.js"
+  },
+  {
+    "revision": "3bde407541893afde1ba",
+    "url": "/player-of-numbers/static/js/runtime-main.b226ac98.js"
+  },
+  {
+    "revision": "08846b1af88731ec8de0e7cc67713dde",
+    "url": "/player-of-numbers/static/media/radiance.08846b1a.svg"
+  }
+]);
